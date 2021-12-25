224bb5b3ff840fc5c96643e6a4af69f422cf6ba6
Viktor Shevchuk <viktor_shevchuk@epam.com>
2021-12-25 09:22:33 +0200

    Updates

---
+++ precache-manifest.bd2d51dab187c277b3154c5bbaefeba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8bb773eb77748f26941b6c38a217fd0",
+    "url": "/ingredients/index.html"
+  },
+  {
+    "revision": "92a3ef998ad01d5e10ed",
+    "url": "/ingredients/static/css/main.f942fe89.chunk.css"
+  },
+  {
+    "revision": "c42d048f30d5bea979bc",
+    "url": "/ingredients/static/js/2.296600b9.chunk.js"
+  },
+  {
+    "revision": "92a3ef998ad01d5e10ed",
+    "url": "/ingredients/static/js/main.16416c0a.chunk.js"
+  },
+  {
+    "revision": "9e449a57ea4ad0e1f6ce",
+    "url": "/ingredients/static/js/runtime~main.b320f567.js"
+  }
+]);
